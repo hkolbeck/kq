f93b96d806d46cea84ea16ac53b8de82b25e9539
Cory <cory.kolbeck@streem.pro>
2021-01-20 09:27:46 -0800

    Initial move over from kdl4j repo

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    jacoco
+    `maven-publish`
+    kotlin("jvm") version "1.4.30-M1"
+}
+
+group = "dev.hbeck.kdl"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/hkolbeck/kdl4j")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = false
+        html.destination = file("${buildDir}/jacoco/coverage")
+    }
+}
+
+dependencies {
+    testImplementation("junit", "junit", "4.12")
+    testImplementation("org.mockito", "mockito-core", "3.7.7")
+    implementation(kotlin("stdlib-jdk8"))
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
